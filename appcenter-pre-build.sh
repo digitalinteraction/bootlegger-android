14ac485662e9cb392a2a45c0e045f67289551600
ntlb1 <>
2019-01-25 09:53:21 +0000

    google json env

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,11 @@
+GOOGLE_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/Bootleg.Android/google-services.json
+
+if [ ! -e "$GOOGLE_JSON_FILE" ]
+then
+    echo "Writing Google Json"
+    echo "$GOOGLE_JSON" > $GOOGLE_JSON_FILE
+    sed -i -e 's/\\"/'\"'/g' $GOOGLE_JSON_FILE
+
+    echo "File content:"
+    cat $GOOGLE_JSON_FILE
+fi
