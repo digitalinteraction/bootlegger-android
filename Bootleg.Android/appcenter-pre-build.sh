6454c8304b6a552717e9e2a85d9ff4e5dde6ba60
Tom Bartindale <tom.bartindale@monash.edu>
2019-07-30 14:46:38 +1000

    update build script

--- Bootleg.Android/appcenter-pre-build.sh
+++ Bootleg.Android/appcenter-pre-build.sh
@@ -1,40 +1,47 @@
 #!/usr/bin/env bash
-GOOGLE_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/Bootleg.Android/google-services.json
-OURSTORY_VERSION_FILE=$APPCENTER_SOURCE_DIRECTORY/Bootleg.Android/BuildVariants/Versions/OurStory.t4
+# GOOGLE_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/Bootleg.Android/google-services.json
+# OURSTORY_VERSION_FILE=$APPCENTER_SOURCE_DIRECTORY/Bootleg.Android/BuildVariants/Versions/OurStory.t4
 
-if [ ! -n "$GOOGLE_JSON" ]
-then
-    echo "You need define the GOOGLE_JSON variable in App Center"
-    exit
-fi
+# if [ ! -n "$GOOGLE_JSON" ]
+# then
+#     echo "You need define the GOOGLE_JSON variable in App Center"
+#     exit
+# fi
 
-if [ ! -n "$OURSTORY_VERSION" ]
-then
-    echo "You need define the OURSTORY_VERSION variable in App Center"
-    exit
-fi
+# if [ ! -n "$OURSTORY_VERSION" ]
+# then
+#     echo "You need define the OURSTORY_VERSION variable in App Center"
+#     exit
+# fi
 
-if [ ! -e "$OURSTORY_VERSION_FILE" ]
-then
-    echo "Writing Version File"
-    mkdir -p $APPCENTER_SOURCE_DIRECTORY/Bootleg.Android/BuildVariants/Versions
-    echo "$OURSTORY_VERSION" > $OURSTORY_VERSION_FILE
-    sed -i -e 's/\\"/'\"'/g' $OURSTORY_VERSION_FILE
+# if [ ! -e "$OURSTORY_VERSION_FILE" ]
+# then
+#     echo "Writing Version File"
+#     mkdir -p $APPCENTER_SOURCE_DIRECTORY/Bootleg.Android/BuildVariants/Versions
+#     echo "$OURSTORY_VERSION" > $OURSTORY_VERSION_FILE
+#     sed -i -e 's/\\"/'\"'/g' $OURSTORY_VERSION_FILE
 
-    echo "File content:"
-    cat $OURSTORY_VERSION_FILE
-fi
+#     echo "File content:"
+#     cat $OURSTORY_VERSION_FILE
+# fi
 
-if [ ! -e "$GOOGLE_JSON_FILE" ]
-then
-    echo "Writing Google Json"
-    echo "$GOOGLE_JSON" > $GOOGLE_JSON_FILE
-    sed -i -e 's/\\"/'\"'/g' $GOOGLE_JSON_FILE
+# if [ ! -e "$GOOGLE_JSON_FILE" ]
+# then
+#     echo "Writing Google Json"
+#     echo "$GOOGLE_JSON" > $GOOGLE_JSON_FILE
+#     sed -i -e 's/\\"/'\"'/g' $GOOGLE_JSON_FILE
 
-    echo "File content:"
-    cat $GOOGLE_JSON_FILE
-fi
+#     echo "File content:"
+#     cat $GOOGLE_JSON_FILE
+# fi
 
+echo "Link Version T4"
+ln -s -t ./Bootleg.Android/BuildVariants/Versions/OurStory.t4 $(T4File.secureFilePath)
+
+echo "Link Google Json"
+ln -s -t ./Bootleg.Android/google-services.json $(GoogleFile.secureFilePath)
+
+echo "Running T4 Process"
 for filename in $(find . -type f -name '*.tt')
 do
     if [[ ${filename: -3} == ".tt" ]]
